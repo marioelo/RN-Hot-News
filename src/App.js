8b8db81a9f5a720e747a34f9eae051cc3dab0d84
Mario Elorza <mario.elorza@gmail.com>
2020-11-30 22:17:00 -0300

    add: cleanup

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text style={{textAlign: 'center'}}>Hot News</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
