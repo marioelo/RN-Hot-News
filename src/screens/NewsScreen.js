9481e7823f4c5891be4e0127f81d5ec851885cfa
Mario Elorza <mario.elorza@gmail.com>
2020-11-30 23:04:01 -0300

    add: main tab bar

---
+++ src/screens/NewsScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    Text,
+    SafeAreaView,
+} from 'react-native';
+
+const NewsScreen = () => {
+
+    return (
+        <SafeAreaView>
+            <Text style={{textAlign: "center"}}>News Screen</Text>
+        </SafeAreaView>
+    );
+}
+
+
+export default NewsScreen;
