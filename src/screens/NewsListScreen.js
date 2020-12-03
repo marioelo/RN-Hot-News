82f4f61882a21222a195359f82b34cbf86fe3588
Mario Elorza <mario.elorza@gmail.com>
2020-12-02 22:30:32 -0300

    add: error feedback

--- src/screens/NewsListScreen.js
+++ src/screens/NewsListScreen.js
@@ -5,6 +5,7 @@ import {
 } from 'react-native';
 import { connect } from 'react-redux';
 import NewsItem from '../components/NewsItem';
+import ErrorView from '../components/ErrorView';
 import { fetchNews } from '../store/newsStore';
 import * as statusCode from '../utilities/constants/statusCode';
 
@@ -23,7 +24,7 @@ const NewsListScreen = props => {
     );
 
 
-    return (
+    const listView = (
         <FlatList
             data={props.articles}
             renderItem={renderItem}
@@ -33,6 +34,20 @@ const NewsListScreen = props => {
             onRefresh={() => props.fetchNews()}
         />
     );
+
+
+    const errorView = (
+        <ErrorView onPress={() => props.fetchNews()} />
+    );
+
+
+    switch (props.status) {
+        case statusCode.LOADING:
+        case statusCode.SUCCESS:
+            return listView;
+        default:
+            return errorView;
+    }
 }
 
 
